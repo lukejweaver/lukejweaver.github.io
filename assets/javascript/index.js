e193aaaee1bf9ec3542458c0e9a4640a7520d2fb
Luke Weaver <35945561+lukejweaver@users.noreply.github.com>
2024-05-18 14:55:03 -0400

    Some updates

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,19 +1,47 @@
-function applyAnimations() {
-  var idsToAnimate = {
-    'title': 'enter-bottom',
-    'headshot': 'enter-left',
-    'brief': 'enter-right'
-  }
+var paperOffset = window.innerHeight * 0.40;
 
+var idsToAnimate = {
+  'title': ['enter-bottom'],
+  'headshot': ['enter-left'],
+  'brief': ['enter-right'],
+  'paper1': ['enter-right-toss-1', paperOffset],
+  'paper2': ['enter-right-toss-2', paperOffset - 10],
+  'paper3': ['enter-right-toss-3', paperOffset - 71],
+  'cv': ['enter-right-toss-4', paperOffset - 150]
+}
+
+var highlightToAnimate = [
+  'highlight-1',
+  'highlight-2',
+  'highlight-3'
+]
+
+var highlightOffset = window.innerHeight * 0.45;
+
+function applyAnimations() {
   Object.keys(idsToAnimate).forEach(key => {
     var element = document.getElementById(key);
-    var animation = idsToAnimate[key];
-    var elementY = element.getBoundingClientRect().y;
-    var elementBottomY = element.getBoundingClientRect().height + elementY;
+    var animationProperties = idsToAnimate[key];
+    var animationOffset = animationProperties[1] ? animationProperties[1] : 0;
+    var elementY = element.getBoundingClientRect().y + animationOffset;
+    var elementBottomY = element.getBoundingClientRect().height + elementY + animationOffset;
 
     if (window.innerHeight > elementY && elementY > 0 || window.innerHeight > elementBottomY && elementBottomY > 0) {
-      element.classList.add(animation);
+      delete(idsToAnimate[key]);
+      element.classList.add(animationProperties[0]);
       element.classList.remove('o0');
+    }
+  });
+
+  highlightToAnimate.forEach(key => {
+    var element = document.getElementById(key);
+    var elementY = element.getBoundingClientRect().y + highlightOffset;
+    var elementBottomY = element.getBoundingClientRect().height + elementY + highlightOffset;
+
+    if (window.innerHeight > elementY && elementY > 0 || window.innerHeight > elementBottomY && elementBottomY > 0) {
+      highlightToAnimate.splice(key, 1);
+      element.classList.add('draw-in-left');
+      element.style.backgroundSize = "100%";
     }
   });
 }
@@ -34,6 +62,18 @@ function scrollListener() {
   });
 }
 
+function addCorrectDate() {
+  const date = new Date();
+  let day = date.getDate();
+  let monthName = date.toLocaleString('default', { month: 'long' });
+  let year = date.getFullYear();
+
+  let currentDate = `${monthName} ${day}, ${year}`;
+  const $date = document.getElementById('abstract-date')
+  $date.textContent = currentDate;
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
   scrollListener()
+  addCorrectDate()
 });
